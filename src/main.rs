3cf1e9071df8914fd6e1af0ebeb667c66f797418
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2023-08-19 13:57:57 +0900

    一致する文字列のある行を出力するように

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,19 @@ struct Args {
     path: std::path::PathBuf,
 }
 
+// 実行例: argo run -- version Cargo.lock
 fn main() {
     let args = Args::parse();
 
     println!("pattern = {}", args.pattern);
+
+    // メモ： expect でエラーを出力する方法はあまりきれいじゃないらしい
+    // https://rust-cli.github.io/book/tutorial/impl-draft.html
+    let content = std::fs::read_to_string(&args.path).expect("ファイルが見つかりませんでした");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
