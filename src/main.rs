666e886c2996f1d1c5784fc83f0b4c09737d554c
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2023-08-19 14:39:52 +0900

    例外処理の実装を変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// https://rust-cli.github.io/book/tutorial/index.html
 use clap::Parser;
 
 #[derive(Parser)]
@@ -7,18 +8,23 @@ struct Args {
 }
 
 // 実行例: argo run -- version Cargo.lock
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     println!("pattern: \"{}\"", args.pattern);
 
-    // メモ： expect でエラーを出力する方法はあまりきれいじゃないらしい
-    // https://rust-cli.github.io/book/tutorial/impl-draft.html
-    let content = std::fs::read_to_string(&args.path).expect("ファイルが見つかりませんでした");
+    let result = std::fs::read_to_string(&args.path);
+
+    let content = match result {
+        Ok(content) => { content },
+        Err(error) => { return Err(error.into()); },
+    };
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+
+    Ok(())
 }
